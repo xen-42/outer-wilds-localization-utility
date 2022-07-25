b50f41515bc3a1c978a23130cb68a2f66e112002
Noah Pilarski <mehmehmeh313@gmail.com>
2022-07-25 00:12:52 -0400

    Ignore case

--- LocalizationUtility/LocalizationAPI.cs
+++ LocalizationUtility/LocalizationAPI.cs
@@ -8,7 +8,7 @@ namespace LocalizationUtility
         public void RegisterLanguage(ModBehaviour mod, string name, string translationPath) 
             => LocalizationUtility.Instance.RegisterLanguage(mod, name, translationPath, TextTranslation.Language.ENGLISH);
         public void RegisterLanguage(ModBehaviour mod, string name, string translationPath, string languageToReplace)
-            => LocalizationUtility.Instance.RegisterLanguage(mod, name, translationPath, (TextTranslation.Language) Enum.Parse(typeof(TextTranslation.Language), languageToReplace));
+            => LocalizationUtility.Instance.RegisterLanguage(mod, name, translationPath, (TextTranslation.Language) Enum.Parse(typeof(TextTranslation.Language), languageToReplace, true));
         public void AddLanguageFont(ModBehaviour mod, string name, string assetBundlePath, string fontPath)
             => LocalizationUtility.Instance.AddLanguageFont(mod, name, assetBundlePath, fontPath);
         public void AddLanguageFixer(string name, Func<string, string> fixer)
